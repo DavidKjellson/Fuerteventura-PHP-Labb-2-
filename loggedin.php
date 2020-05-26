a710e8903d1ddba2cb174866e75f3212a605642b
Tilda Sandqvist <tilda.sandqvist@live.se>
2020-05-26 10:19:12 +0200

    Gjort en inloggningssida för admins.

---
+++ loggedin.php
@@ -0,0 +1,64 @@
+<?php
+session_start(); 
+
+$users = []; 
+
+$username= ($SESSION['username']);
+$password= ($SESSION['password']);
+
+$database = new PDO("mysql:host=localhost;dbname=php-labb-2", "root","root"); 
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Document</title>
+</head>
+<body>
+
+<form action="index.php" method="post">
+<input type="submit" name="log-out" value="Logga ut">
+</form>
+<br>
+
+
+<div class="welcomeAdmin">
+<?php
+// Skriver ut alla användare
+    if(isset($users)){
+        echo 'Välkommen!';
+    }
+    ?>
+</div>
+
+    
+ <?php
+// Log out 
+if(isset($_POST['log-out'])){ 
+  session_destroy();
+  header('location: index.php');
+}
+  ?>
+</body>
+</html>
+
+
+
+<!-- 
+// $count=mysql_fetch_array($result);
+
+// if($count > 0){
+//   session_register("username");
+//   session_register("password");
+//   header("location:success.php");
+// } else {
+//   echo 'Wrong Username or Password! Return to <a href="index.html">login</a>';
+//   }
+?> -->
+
+
+
