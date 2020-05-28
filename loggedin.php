13dc047aee1f56d2f4d86358afa5a58781d8d9b6
popka56 <popka56@github.com>
2020-05-28 13:26:12 +0200

    Kan skapa nya användare

--- loggedin.php
+++ loggedin.php
@@ -101,12 +101,61 @@ function displayData()
   $database = null;
 }
 
+function addNewUser($username, $password, $firstName, $lastName, $payment, $activity1, $activity2, $activity3){
+  //logga in i databasen
+  $dbHostname = "localhost";
+  $dbUsername = "root";
+  $dbPassword = "root";
+
+  //Räkna hur många aktiviteter användaren är med i
+  $activities = array();
+  if($activity1){
+      array_push($activities, $activity1);
+  }
+  if($activity2){
+      array_push($activities, $activity2);
+  }
+  if($activity3){
+      array_push($activities, $activity3);
+  }
+  $numberOfActivities = count($activities);
+
+  try {
+      //koppla upp oss till databasen
+      $database = new PDO("mysql:host=$dbHostname;dbname=php-labb-2", $dbUsername, $dbPassword);
+      $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      
+      //lägg till användaren
+      $database->exec("INSERT
+                       INTO `users`(`username`, `password`, `full_name`, `paid_the_fee`, `number_of_activities`)
+                       VALUES ('$username', '$password', '$firstName $lastName', '$payment', '$numberOfActivities')");
+
+      //lägg till användarens aktiviteter
+      if($numberOfActivities > 0){
+          $userId = $database->lastInsertId();
+          foreach($activities as $activity){
+              $database->exec("INSERT
+                               INTO `user_activities`(`ua_user`, `ua_activity`)
+                               VALUES ('$userId', '$activity')");
+          }
+      }
+      
+      //ladda om sidan så användaren kan se resultatet direkt
+      header("Refresh:0");
+  }
+  catch(PDOException $e){
+      
+      echo "⚠️ Connection failed: " . $e->getMessage();
+  
+  }
+}
+
 //Ta bort en användare
 function deleteUser($userId)
 {
   $hostname = "localhost";
-  $username = "admin";
-  $password = "admin";
+  $username = "root";
+  $password = "root";
   try {
     //koppla upp oss till databasen
     $database = new PDO("mysql:host=$hostname;dbname=php-labb-2", $username, $password);
@@ -117,8 +166,11 @@ function deleteUser($userId)
                              FROM `users`
                              INNER JOIN `user_activities` ON `users`.`user_id` = `user_activities`.`ua_user`
                              WHERE `user_id`= $userId");
-  } catch (PDOException $e) {
 
+    //ladda om sidan så användaren kan se resultatet direkt
+    header("Refresh:0");
+
+  } catch (PDOException $e) {
     echo "⚠️ Connection failed: " . $e->getMessage();
   }
 }
@@ -126,8 +178,8 @@ function deleteUser($userId)
 function updateUser()
 {
   $hostname = "localhost";
-  $username = "admin";
-  $password = "admin";
+  $username = "root";
+  $password = "root";
 
   try {
     $database = new PDO("mysql:host=$hostname;dbname=php-labb-2", $username, $password);
@@ -158,6 +210,7 @@ function updateUser()
 </head>
 
 <body>
+<header style="position:absolute;left:0;top:0;overflow:auto;width:100%;height:10px;background-color: rgb(255, 92, 92);"></header>
 
   <div class="welcomeAdmin">
     <?php
@@ -169,18 +222,68 @@ function updateUser()
 
     <table>
       <?php
+      //visa datan högst upp
       if (isset($users)) {
         displayData();
       }
       ?>
     </table>
+    
+    <!--Knapp för att skapa nya användare under datan-->
+    <form method="post">
+      <input type="submit" value="Lägg till ny användare" name="newUser">
+    </form>
 
     <?php
-    if (isset($_POST['deleteUser'])) {
-      deleteUser($_POST['userId']);
-    }
+      //formulär för att skapa ny användare
+      if (isset($_POST['newUser'])) {
+        echo '
+        <form method="post">
+      
+        <p>Användarnamn och lösenord*</p>
+        <input type="text" placeholder="Användarnamn" name="username" required>
+        <input type="password" placeholder="Lösenord" name="password" required>
+      
+        <p>Namn*</p>
+        <input type="text" placeholder="Förnamn" name="firstName" required>
+        <input type="text" placeholder="Efternamn" name="lastName" required>
+      
+        <p>Har betalt medlemsavgiften?*</p>
+        <select name="payment">
+            <option value="0">Nej</option>
+            <option value="1">Ja</option>
+        </select>
+      
+        <p>Aktiviteter</p>
+        <input type="checkbox" name="activity1" value="1">
+        <label for="activity1">Fotboll</label>
+        <input type="checkbox" name="activity2" value="2">
+        <label for="activity2">Skidor</label>
+        <input type="checkbox" name="activity3" value="3">
+        <label for="activity3">Gymnastik</label>
+
+        <br>
+        <p>* = Obligatoriskt</p>
+        <br>
+      
+        <input type="submit" value="Skicka" name="addNewUser">
+        </form>
+        ';
+      }
+
+      //kallar på funktionen att skapa den nya användaren
+      if (isset($_POST['addNewUser'])) {
+        unset($_POST['newUser']);
+        addNewUser($_POST['username'], $_POST['password'], $_POST['firstName'], $_POST['lastName'], $_POST['payment'], $_POST['activity1'], $_POST['activity2'], $_POST['activity3']);
+      }
+
+      //kallar på funktionen att ta bort användaren
+      if (isset($_POST['deleteUser'])) {
+        deleteUser($_POST['userId']);
+      }
     ?>
 
+    <!--Knapp att logga ut från sidan-->
     <form action="index.php" method="post">
       <input type="submit" name="log-out" value="Logga ut">
     </form>
@@ -195,7 +298,6 @@ function updateUser()
     header('location: index.php');
   }
   ?>
-
 </body>
 
 </html>
